60be830f1cf130ae60ebd8d02de4a77a0506d5fe
Amit Dafda <dafdaamit206@gmail.com>
2025-08-16 12:36:16 +0000

    modified package and build errors

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,13 +1,12 @@
-
 // IMPORTANT: This file is a convention for hosting Genkit flows on Firebase App Hosting.
 //
 // It is NOT a conventional Next.js API route that you would create for your app.
 //
 // This file is used by the Genkit framework to handle requests to your flows.
 // You should not need to edit this file.
-import {genkit} from 'genkit';
+import {createApiRoute} from 'genkitx-next';
+import {ai} from '@/ai/genkit';
 
-export const dynamic = 'force-dynamic';
-export const maxDuration = 60; // Set a 60-second timeout
-
-export {genkit as GET, genkit as POST};
+export const {GET, POST} = createApiRoute({
+  ai,
+});
